7a063a30af453dbaa2417e82ceb551cec5c68a30
Vic146 <vickieteran146@gmail.com>
2023-11-01 22:57:55 -0500

    Revert "Merge pull request #1 from vic146/animation"
    This reverts commit 949fc429705e82692a8d5518b588a02d84b1cfc3, reversing
    changes made to d13eae558c5c6fb957547cd03f15e27e47e149e7.

--- FinalProject/Assets/Scripts/PlayerInput.cs
+++ FinalProject/Assets/Scripts/PlayerInput.cs
@@ -1,60 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;
-// using UnityEngine.SceneManagement;
-
-
-// public class PlayerInput : MonoBehaviour
-// { 
-//    [SerializeField] UI_Inventory uiInventory;
-//    [SerializeField] DiscreteMovement movement;
-   
-//    private Inventory inventory;
-
-//     void Awake(){
-//         Debug.Log("playerInput");
-//         movement = GetComponent<DiscreteMovement>();
-//         inventory = new Inventory();
-//         uiInventory.SetInventory(inventory);
-
-//         /*ItemWorld.SpawnItemWorld(new Vector3(0, -3), new Item {itemType = Item.ItemType.Chair, amount = 1});
-//         ItemWorld.SpawnItemWorld(new Vector3(1, -3), new Item {itemType = Item.ItemType.Chair, amount = 1});
-//         ItemWorld.SpawnItemWorld(new Vector3(3, -3), new Item {itemType = Item.ItemType.Chair, amount = 1});*/
-//     }
-
-//     private void OnTriggerEnter2D(Collider2D collider){
-//         ItemWorld itemWorld = collider.GetComponent<ItemWorld>();
-//         if (itemWorld != null){
-//             inventory.AddItem(itemWorld.GetItem());
-//             itemWorld.DestroySelf();
-//         }
-//     }
-
-//     void Update(){
-    
-//         if(Input.GetKey(KeyCode.A)){ 
-//             movement.MoveRB(new Vector3(-1, 0, 0));
-//         }
-//         else if(Input.GetKey(KeyCode.D)){
-//             movement.MoveRB(new Vector3(1, 0, 0));
-//         }
-//         else{
-//             movement.Stop();
-//         }
-
-//         if (Input.GetKeyDown(KeyCode.Space)){
-//             movement.Jump();
-//         }
-//         if(transform.position.y < -8){
-//         SceneManager.LoadScene("Main Menu");
-//         }
-//     }
-    
-
-// }
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -67,15 +10,12 @@ public class PlayerInput : MonoBehaviour
    [SerializeField] UI_Inventory uiInventory;
    [SerializeField] DiscreteMovement movement;
    private Inventory inventory;
-   
 
     void Awake(){
         Debug.Log("playerInput");
-        
         movement = GetComponent<DiscreteMovement>();
-        inventory =  Inventory.Instance;
-
-        UI_Inventory.Instance.SetInventory(inventory);
+        inventory = new Inventory();
+        uiInventory.SetInventory(inventory);
 
         /*ItemWorld.SpawnItemWorld(new Vector3(0, -3), new Item {itemType = Item.ItemType.Chair, amount = 1});
         ItemWorld.SpawnItemWorld(new Vector3(1, -3), new Item {itemType = Item.ItemType.Chair, amount = 1});
@@ -105,11 +45,8 @@ public class PlayerInput : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)){
             movement.Jump();
         }
-        if(transform.position.y < -8){    
-        Inventory.Instance.ResetTotalItemCount(); 
-        UI_Inventory.Instance.ClearInventoryItems(); 
+        if(transform.position.y < -8){
         SceneManager.LoadScene("Main Menu");
-        
         }
     }
     
