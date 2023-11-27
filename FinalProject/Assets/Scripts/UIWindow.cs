dc8999dda06e45080cfa64b1b63652b3346bd012
vic146 <124853054+vic146@users.noreply.github.com>
2023-11-27 15:27:40 -0600

    Update Particle/options menu

---
+++ FinalProject/Assets/Scripts/UIWindow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWindow : MonoBehaviour
+{
+    public void Show(){
+        GetComponent<Canvas>().enabled = true;
+    }
+
+    public void Hide(){
+        GetComponent<Canvas>().enabled = false;
+    }
+}
